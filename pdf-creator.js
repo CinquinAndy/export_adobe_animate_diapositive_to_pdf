0bd8df5618da87749a6ce2464aa7737101fd2667
Cinquin Andy <andy.cinquin@infotel.com>
2023-12-03 16:48:27 +0100

    🔧 Add PDF creation functionality
    This commit adds the ability to create a PDF document from a folder of PNG images. The `createPDF` function takes in the path to the folder containing the images and the output path for the resulting PDF file. It reads each image file, embeds it into a new page in the PDF document, and saves the final PDF file to the specified output path. If successful, it logs a success message; otherwise, it logs an error message.

---
+++ pdf-creator.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const { PDFDocument } = require('pdf-lib');
+
+async function createPDF(imagesFolder, outputPath) {
+    const pdfDoc = await PDFDocument.create();
+
+    const imageFiles = fs.readdirSync(imagesFolder).filter(file => file.endsWith('.png'));
+
+    for (const file of imageFiles) {
+        const imagePath = path.join(imagesFolder, file);
+        const imageBytes = fs.readFileSync(imagePath);
+        const image = await pdfDoc.embedPng(imageBytes);
+
+        const page = pdfDoc.addPage([image.width, image.height]);
+        page.drawImage(image, { x: 0, y: 0 });
+    }
+
+    const pdfBytes = await pdfDoc.save();
+    fs.writeFileSync(outputPath, pdfBytes);
+}
+
+createPDF(process.env.SCREENSHOT_PATH, path.join(process.env.OUTPUT_FILE_PATH, 'diapositives.pdf'))
+    .then(() => console.log('PDF created successfully.'))
+    .catch(err => console.error('Error creating PDF:', err));
