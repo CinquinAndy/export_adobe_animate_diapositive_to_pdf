83eb28f931552d010d22210c10ecaecce20ca317
Cinquin Andy <andy.cinquin@infotel.com>
2023-12-03 16:24:58 +0100

    🔧 Update screenshot-captures.js
    - Launch Puppeteer with headless mode disabled
    - Set a default timeout for the page
    - Set the viewport size to 1024x766
    - Add delay before taking initial screenshot
    - Add code to locate and click on specific elements on the page
    - Take screenshots for each slide in a loop, with delays between each slide
    - Close the browser at the end

--- screenshot-captures.js
+++ screenshot-captures.js
@@ -8,14 +8,67 @@ function delay(time) {
 }
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    const timeout = 5000;
+    const totalSlides = 375;
+    page.setDefaultTimeout(timeout);
+    await page.setViewport({ width: 1024, height: 766 });
     await page.goto(process.env.SLIDESHOW_URL); // Use URL from .env file
 
-    for (let i = 0; i < 375; i++) {
+    await delay(4000);
+    await page.screenshot({ path: `init.png` }); // Use path from .env file
+    {
+        const targetPage = page;
+        await puppeteer.Locator.race([
+            targetPage.locator('::-p-aria(Médecine)'),
+            targetPage.locator('#form1'),
+            targetPage.locator('::-p-xpath(//*[@id=\\"form1\\"])'),
+            targetPage.locator(':scope >>> #form1')
+        ])
+            .setTimeout(timeout)
+            .click({
+                offset: {
+                    x: 4.630126953125,
+                    y: 3.221923828125,
+                },
+            });
+    }
+    await delay(4000);
+    {
+        const targetPage = page;
+        await puppeteer.Locator.race([
+            targetPage.locator('#canvas'),
+            targetPage.locator('::-p-xpath(//*[@id=\\"canvas\\"])'),
+            targetPage.locator(':scope >>> #canvas')
+        ])
+            .setTimeout(timeout)
+            .click({
+                offset: {
+                    x: 809.2755737304688,
+                    y: 21.09659194946289,
+                },
+            });
+    }
+
+    for (let i = 0; i < totalSlides; i++) {
         await delay(4000); // Wait for animations
         await page.screenshot({ path: `${process.env.SCREENSHOT_PATH}${i}.png` }); // Use path from .env file
-        await page.click(process.env.CANVAS_SELECTOR); // Use selector from .env file
+        {
+            const targetPage = page;
+            await puppeteer.Locator.race([
+                targetPage.locator('#canvas'),
+                targetPage.locator('::-p-xpath(//*[@id=\\"canvas\\"])'),
+                targetPage.locator(':scope >>> #canvas')
+            ])
+                .setTimeout(timeout)
+                .click({
+                    offset: {
+                        x: 809.2755737304688,
+                        y: 21.09659194946289,
+                    },
+                });
+        }
     }
 
     await browser.close();
